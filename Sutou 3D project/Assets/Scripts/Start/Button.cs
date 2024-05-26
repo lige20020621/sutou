6d3712bd60de687b39a64c2ec50b93ffde5a2c2d
WesleyWong <wesley16838@gmail.com>
2024-05-25 23:08:02 -0500

    add move up feature

--- Sutou 3D project/Assets/Scripts/Start/Button.cs
+++ Sutou 3D project/Assets/Scripts/Start/Button.cs
@@ -6,6 +6,16 @@ using UnityEngine.SceneManagement;
 
 public class Button : MonoBehaviour
 {
+    int count = 0;
+    public float moveDistance = 50f; // Distance to move up
+    private Vector3 originalPosition; // Store the original position of the GameObject
+
+    void Start()
+    {
+        // Store the original position
+        originalPosition = transform.position;
+    }
+
     public void SwitchScene1()
     {
         SceneManager.LoadScene(1);
@@ -13,6 +23,12 @@ public class Button : MonoBehaviour
 
     public void SwitchScene3()
     {
-        SceneManager.LoadScene(3);
+        if(count < 3) {
+            transform.Translate(Vector3.up * moveDistance);
+            count++;
+        } else {
+            count = 0;
+            SceneManager.LoadScene(3);
+        }
     }
 }
